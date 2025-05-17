495d1405e58da3ff960c6283c0bb9696ca299938
BassemBG <bgbassem8@gmail.com>
2025-05-17 16:50:35 +0100

    added tests for gateway and updated microservice dockerfile + requirements to work when i build docker container

--- microservices/speech_emotion_microservice/app.py
+++ microservices/speech_emotion_microservice/app.py
@@ -1,8 +1,9 @@
 from flask import Flask
 from routes.speech_routes import speech_bp
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app)
 # Register the speech blueprint
 app.register_blueprint(speech_bp, url_prefix='/api/speech')
 
