abc4c1c57751674b433c0dbb952285393b1bc37d
bechir2003 <bechir.mejri@esprit.tn>
2025-05-19 23:32:27 +0100

    brain2text

--- microservices/brain_to_text_microservice/app.py
+++ microservices/brain_to_text_microservice/app.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from routes.brain2text_routes import brain_to_text_blueprint
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+
 app.register_blueprint(brain_to_text_blueprint)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5010)
+    app.run(host='0.0.0.0', port=5000)
